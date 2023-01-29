8788ef94288fc5c091af4eaf68aca7211a74c1ae
Youssef Wadie <youssefwadie14@gmail.com>
2023-01-29 23:45:50 +0200

    controllers refactor

--- readwithme/src/main/java/com/github/youssefwadie/readwithme/book/BookController.java
+++ readwithme/src/main/java/com/github/youssefwadie/readwithme/book/BookController.java
@@ -2,14 +2,15 @@ package com.github.youssefwadie.readwithme.book;
 
 
 import lombok.RequiredArgsConstructor;
+import lombok.val;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.reactive.result.view.Rendering;
 import reactor.core.publisher.Mono;
 import reactor.core.publisher.SynchronousSink;
 
@@ -27,30 +28,43 @@ public class BookController {
     private final BookRepository bookRepository;
 
     @GetMapping(path = "/{bookId}", produces = MediaType.TEXT_HTML_VALUE)
-    public Mono<String> getBook(@PathVariable(name = "bookId") String bookId, Model model,
-                                @AuthenticationPrincipal Mono<OAuth2User> principalMono) {
-
+    public Mono<Rendering> getBook(@PathVariable(name = "bookId") String bookId,
+                                   @AuthenticationPrincipal Mono<OAuth2User> principalMono) {
         return bookRepository.findById(bookId)
-                .flatMap(book -> {
-                    var coverImageUrl = "/images/no-image.png";
-
-                    if (book.getCoversId() != null && !book.getCoversId().isEmpty()) {
-                        coverImageUrl = String.format("%s/%s-L.jpg", COVER_IMAGE_ROOT, book.getCoversId().get(0));
-                    }
-
-                    model.addAttribute("coverImage", coverImageUrl);
-                    model.addAttribute("book", book);
-
-                    if (principalMono == null) return Mono.just(BOOK_TEMPLATE_NAME);
-
-                    return principalMono.handle((OAuth2User oAuth2User, SynchronousSink<String> synchronousSink) -> {
-                        if (Objects.nonNull(oAuth2User.getAttribute("login"))) {
-                            model.addAttribute("loginId", oAuth2User.getAttribute("login"));
-                        }
-                        synchronousSink.next(BOOK_TEMPLATE_NAME);
-                    });
-                })
-                .defaultIfEmpty(BOOK_NOT_FOUND_TEMPLATE_NAME);
+                .flatMap(book -> mapBookAndPrincipalToView(book, principalMono))
+                .switchIfEmpty(bookNotFound());
     }
 
+    private Mono<Rendering> mapBookAndPrincipalToView(Book book, Mono<OAuth2User> principalMono) {
+
+        var coverImageUrl = "/images/no-image.png";
+
+        if (book.getCoversId() != null && !book.getCoversId().isEmpty()) {
+            coverImageUrl = String.format("%s/%s-L.jpg", COVER_IMAGE_ROOT, book.getCoversId().get(0));
+        }
+
+        val renderingBuilder = Rendering.view(BOOK_TEMPLATE_NAME);
+
+        renderingBuilder.modelAttribute("coverImage", coverImageUrl);
+        renderingBuilder.modelAttribute("book", book);
+
+        return addPrincipal(renderingBuilder, principalMono);
+    }
+
+    private Mono<Rendering> addPrincipal(Rendering.Builder<?> renderingBuilder, Mono<OAuth2User> principalMono) {
+        if (principalMono == null) return Mono.just(renderingBuilder.build());
+
+        return principalMono.handle((OAuth2User oAuth2User, SynchronousSink<Rendering> synchronousSink) -> {
+            val loginId = oAuth2User.getAttribute("login");
+            if (Objects.nonNull(loginId)) {
+                renderingBuilder.modelAttribute("loginId", loginId);
+            }
+            synchronousSink.next(renderingBuilder.build());
+        });
+
+    }
+
+    private Mono<Rendering> bookNotFound() {
+        return Mono.fromSupplier(() -> Rendering.view(BOOK_NOT_FOUND_TEMPLATE_NAME).build());
+    }
 }
--- readwithme/src/main/java/com/github/youssefwadie/readwithme/search/SearchController.java
+++ readwithme/src/main/java/com/github/youssefwadie/readwithme/search/SearchController.java
@@ -1,27 +1,33 @@
 package com.github.youssefwadie.readwithme.search;
 
 import lombok.RequiredArgsConstructor;
+import lombok.val;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.reactive.result.view.Rendering;
 import reactor.core.publisher.Mono;
 
 @RequiredArgsConstructor
 @Controller
 @RequestMapping("search")
 public class SearchController {
+    private static final String SEARCH_TEMPLATE_NAME = "search";
     private final SearchService searchService;
 
     @GetMapping(path = "", produces = MediaType.TEXT_HTML_VALUE)
-    public Mono<String> getSearchResults(@RequestParam("query") String query, Model model) {
-        return searchService.search(query)
-                .map(searchResult -> {
-                    model.addAttribute("searchResults", searchResult.getDocs());
-                    model.addAttribute("numFound", searchResult.getNumFound());
-                    return "search";
-                });
+    public Mono<Rendering> getSearchResults(@RequestParam("query") String query) {
+        val searchResultMono = searchService.search(query);
+        return searchResultMono
+                .map(this::mapSearchResultsToRendering);
+    }
+
+    private Rendering mapSearchResultsToRendering(SearchResult searchResult) {
+        return Rendering.view(SEARCH_TEMPLATE_NAME)
+                .modelAttribute("searchResults", searchResult.getDocs())
+                .modelAttribute("numFound", searchResult.getNumFound())
+                .build();
     }
 }
