4efe5d4a11c7a8bbf05bd4b41db289bfba9a838e
Youssef Wadie <youssefwadie14@gmail.com>
2023-01-29 14:59:31 +0200

    update metafiles

--- src/main/java/io/javabrains/betterreads/book/BookRepository.java
+++
@@ -1,9 +0,0 @@
-package io.javabrains.betterreads.book;
-
-import org.springframework.data.cassandra.repository.CassandraRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BookRepository extends CassandraRepository<Book, String> {
-    
-}
--- src/main/java/io/javabrains/betterreads/userbooks/UserBooksRepository.java
+++
@@ -1,7 +0,0 @@
-package io.javabrains.betterreads.userbooks;
-
-import org.springframework.data.cassandra.repository.CassandraRepository;
-
-public interface UserBooksRepository extends CassandraRepository<UserBooks, UserBooksPrimaryKey> {
-    
-}
